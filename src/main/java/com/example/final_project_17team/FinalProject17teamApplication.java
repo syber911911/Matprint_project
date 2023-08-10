023d4aae7d0a49fded8784fd71dcd66e55edd16d
ubiies <alal55al9@naver.com>
2023-08-10 14:13:36 +0900

    first commit

---
+++ src/main/java/com/example/final_project_17team/FinalProject17teamApplication.java
@@ -0,0 +1,13 @@
+package com.example.final_project_17team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProject17teamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProject17teamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/final_project_17team/FinalProject17teamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.final_project_17team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProject17teamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
