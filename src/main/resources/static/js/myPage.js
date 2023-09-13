24f5dc0fe2ae851fd7e7b70197608b5eb09fa365
yun seok <str17gogo@naver.com>
2023-09-14 03:23:13 +0900

    feat: 세션 스토리지 수정 -content: 수정

--- src/main/resources/static/js/myPage.js
+++ src/main/resources/static/js/myPage.js
@@ -15,6 +15,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     const uploadBtn = document.getElementById("upload");
     const changeContentBox = document.getElementById('changecontentBox');
     const token = localStorage.getItem('token') || sessionStorage.getItem('token');
+    console.log(token);
     const HttpHandler = new httpModule.HttpHandler(token);
     const User = new userModule.User(token);
     const { likePostTemplate, postTemplate, profileTemplate, uploadTemplate, updateFormTemplate } = templateModule;
