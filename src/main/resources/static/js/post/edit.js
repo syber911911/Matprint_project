54ff269fe38f48b674f828bcf506e6529a7bbd1e
ubiies <alal55al9@naver.com>
2023-09-14 04:32:03 +0900

    fix : 로그아웃 시에도 댓글 보이게 수정

--- src/main/resources/static/js/post/edit.js
+++ src/main/resources/static/js/post/edit.js
@@ -1,9 +1,14 @@
 function isAuthor(postUsername) {
     const currentToken = localStorage.getItem('token') || sessionStorage.getItem('token');
-    const payload = decodeJwtToken(currentToken); // JWT 토큰 디코딩하여 payload 추출
+
+    if (!currentToken) {
+        return false;
+    }
+
+    const payload = decodeJwtToken(currentToken);
     const currentUsername = payload.sub; // 토큰에서 username 추출
 
-    return postUsername === currentUsername; // 글쓴이와 현재 로그인된 사용자가 일치하면 true 반환
+    return postUsername === currentUsername;
 }
 
 // 수정 버튼 클릭 시 동작
@@ -112,21 +117,23 @@ backButton.addEventListener('click', function() {
 
 window.onload = function() {
     checkLogin();
-    fetchComments();
     fetchPostDetail()
         .then(postUsername => {
-            console.log("Post author: ", postUsername); // 게시글 작성자 출력
+            console.log("Post author: ", postUsername);
             const currentToken = localStorage.getItem('token') || sessionStorage.getItem('token');
-            const payload = decodeJwtToken(currentToken); // JWT 토큰 디코딩하여 payload 추출
-            const currentUsername = payload.sub; // 토큰에서 username 추출
-            console.log("Logged in user: ", currentUsername); // 현재 로그인된 사용자 출력
+            if (currentToken) {  // 로그인 상태일 때만 실행
+                const payload = decodeJwtToken(currentToken);
+                const currentUsername = payload.sub; // 토큰에서 username 추출
+                console.log("Logged in user: ", currentUsername);
 
-            if (isAuthor(postUsername)) {
-                const postButtons = document.getElementById('post-buttons');
-                postButtons.style.display = 'block';
+                if (isAuthor(postUsername)) {
+                    const postButtons = document.getElementById('post-buttons');
+                    postButtons.style.display = 'block';
+                }
             }
         })
         .catch(error => console.log(error));
+    fetchComments();
 };
 
 
