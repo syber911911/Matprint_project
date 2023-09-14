c7988c794fdcbe5a555210e2b1b189605ad4d102
ubiies <alal55al9@naver.com>
2023-09-14 20:13:35 +0900

    fix : reissue 설정 중

--- src/main/resources/static/js/users.js
+++ src/main/resources/static/js/users.js
@@ -57,14 +57,3 @@ function checkLogin() {
 window.onload = function () {
     checkLogin();
 }
-
-// jwt 토큰 디코딩
-function decodeJwtToken(token) {
-    const base64Url = token.split('.')[1];
-    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-    console.log(jsonPayload);
-    return JSON.parse(jsonPayload);
-}
