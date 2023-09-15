6db053e58aae360179be34ff5a9205b2dc312b8a
sdw504512 <sdw504512@gmail.com>
2023-09-15 09:10:20 +0900

    fix: userController localhost 제거

--- src/main/java/com/example/final_project_17team/user/service/UserService.java
+++ src/main/java/com/example/final_project_17team/user/service/UserService.java
@@ -63,7 +63,6 @@ public class UserService implements UserDetailsManager {
                     .secure(true)
                     .sameSite("Lax")
                     .domain("matprint.site")
-                    .domain("localhost")
                     .path("/")
                     .maxAge(3600 * 24 * 14)
                     .build();
@@ -73,32 +72,10 @@ public class UserService implements UserDetailsManager {
                     .secure(true)
                     .sameSite("Lax")
                     .domain("matprint.site")
-                    .domain("localhost")
                     .path("/")
                     .build();
         }
         response.addHeader("Set-Cookie", refreshTokenCookie.toString());
-    }
-
-    public void setAutoLoginCookie(String autoLogin, HttpServletResponse response) {
-        ResponseCookie autoLoginCookie = null;
-        if (autoLogin.equals("T")) {
-            autoLoginCookie = ResponseCookie.from("AUTO_LOGIN", "T")
-                    .sameSite("Lax")
-                    .domain("matprint.site")
-                    .domain("localhost")
-                    .path("/")
-                    .maxAge(3600 * 24 * 14)
-                    .build();
-        } else {
-            autoLoginCookie = ResponseCookie.from("AUTO_LOGIN", "F")
-                    .sameSite("Lax")
-                    .domain("matprint.site")
-                    .domain("localhost")
-                    .path("/")
-                    .build();
-        }
-        response.addHeader("Set-Cookie", autoLoginCookie.toString());
     }
 
     public JwtTokenInfoDto loginUser(LoginDto request) {
